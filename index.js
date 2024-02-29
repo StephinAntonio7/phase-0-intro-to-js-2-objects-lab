4b048b0d05bbdc640a205b36fd6b29861a4a5329
Stephin Roberts <stephin.roberts@gmail.com>
2024-02-29 15:33:56 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const clone = Object.assign({}, obj);
+    clone[key] = value;
+  
+    return clone;
+  }
+  const employee = {
+    name: 'John Doe',
+    address: '11 Broadway',
+    position: 'Manager'
+  };
+  updateEmployeeWithKeyAndValue(employee, 'position', 'Senior Manager');
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+  
+    // return the updated employee object
+    return obj;
+  }
+  function deleteFromEmployeeByKey(obj, key) {
+    const clone = Object.assign({}, obj);
+    delete clone[key];
+  
+    // return the cloned object
+    return clone;
+  }
+  function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+  
+    // return the modified object
+    return obj;
+  }
